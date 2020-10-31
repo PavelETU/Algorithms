9bb88e84e0502a9e3c23745d927df24e73aea3f5
PavelETU <pavelsuvit@gmail.com>
2020-10-31 22:20:27 +0000

    Refactor to use memorization

--- src/dynamicprogramming/FifteenStep.kt
+++ src/dynamicprogramming/FifteenStep.kt
@@ -6,12 +6,11 @@ import kotlin.test.assertEquals
 // It actually should be triple step but I'm too much into Radiohead for that
 class FifteenStep {
     private fun waysToStep(n: Int): Int {
-        return when(n) {
-            1 -> 1
-            2 -> 2
-            3 -> 4
-            else -> waysToStep(n - 1) + waysToStep(n - 2) + waysToStep(n - 3) + 3
+        val counts: MutableMap<Int, Int> = mutableMapOf(1 to 1, 2 to 2, 3 to 4)
+        for (i in 4..n) {
+            counts[i] = counts[n - 1]!! + counts[n - 2]!! + counts[n - 3]!! + 3
         }
+        return counts[n]!!
     }
 
     @Test
@@ -19,5 +18,6 @@ class FifteenStep {
         assertEquals(1, waysToStep(1))
         assertEquals(2, waysToStep(2))
         assertEquals(4, waysToStep(3))
+        assertEquals(10, waysToStep(4))
     }
 }
