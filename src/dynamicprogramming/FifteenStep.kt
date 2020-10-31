4f0c868485ba00fa3eb8914ee42c31f42e2cbd39
PavelETU <pavelsuvit@gmail.com>
2020-10-31 22:43:35 +0000

    Fix a mistake in the solution and take care of the overflow

--- src/dynamicprogramming/FifteenStep.kt
+++ src/dynamicprogramming/FifteenStep.kt
@@ -5,10 +5,10 @@ import kotlin.test.assertEquals
 
 // It actually should be triple step but I'm too much into Radiohead for that
 class FifteenStep {
-    private fun waysToStep(n: Int): Int {
-        val counts: MutableMap<Int, Int> = mutableMapOf(1 to 1, 2 to 2, 3 to 4)
+    private fun waysToStep(n: Int): Long {
+        val counts: MutableMap<Int, Long> = mutableMapOf(1 to 1, 2 to 2, 3 to 4)
         for (i in 4..n) {
-            counts[i] = counts[n - 1]!! + counts[n - 2]!! + counts[n - 3]!! + 3
+            counts[i] = counts[i - 1]!! + counts[i - 2]!! + counts[i - 3]!!
         }
         return counts[n]!!
     }
@@ -18,6 +18,6 @@ class FifteenStep {
         assertEquals(1, waysToStep(1))
         assertEquals(2, waysToStep(2))
         assertEquals(4, waysToStep(3))
-        assertEquals(10, waysToStep(4))
+        assertEquals(7, waysToStep(4))
     }
 }
